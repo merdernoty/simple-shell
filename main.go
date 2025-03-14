5fd8d0ce50df2e3f40154af069f7330854acc671
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2025-03-15 01:47:02 +0300

    init base shell func: pwd, echo, cat, mkdir, help

--- main.go
+++ main.go
@@ -45,6 +45,52 @@ func ls(args []string) error {
 	return nil
 }
 
+func pwd(args []string) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	fmt.Println(dir)
+	return nil
+}
+
+func echo(args []string) error {
+	fmt.Println(strings.Join(args, " "))
+	return nil
+}
+
+func cat(args []string) error {
+	if len(args) < 1 {
+		return errors.New("filename required")
+	}
+	data, err := os.ReadFile(args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(data))
+	return nil
+}
+
+func mkdir(args []string) error {
+	if len(args) < 1 {
+		return errors.New("directory name required")
+	}
+	return os.Mkdir(args[0], 0755)
+}
+
+func help(args []string) error {
+	fmt.Println("Available сommands:")
+	fmt.Println("cd     - change directory")
+	fmt.Println("ls     - list directory contents")
+	fmt.Println("pwd    - print current working directory")
+	fmt.Println("echo   - print arguments")
+	fmt.Println("cat    - display file content")
+	fmt.Println("mkdir  - create a new directory")
+	fmt.Println("help   - display this help message")
+	fmt.Println("exit   - exit the shell")
+	return nil
+}
+
 func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
@@ -57,6 +103,16 @@ func execInput(input string) error {
 		return os.Chdir(args[1])
 	case "ls":
 		return ls(args[1:])
+	case "pwd":
+		return pwd(args[1:])
+	case "echo":
+		return echo(args[1:])
+	case "cat":
+		return cat(args[1:])
+	case "mkdir":
+		return mkdir(args[1:])
+	case "help":
+		return help(args[1:])
 	case "exit":
 		os.Exit(0)
 	}
